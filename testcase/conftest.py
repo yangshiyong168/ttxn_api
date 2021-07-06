21ebddeda826026c87bcd7e73b86378b0f8aa2d6
e <yangshiyong668@163.com>
2021-07-06 18:43:06 +0800

    add allure

--- testcase/conftest.py
+++ testcase/conftest.py
@@ -1,20 +1,46 @@
 
 import time,os,sys
+from datetime import datetime
+
 import pytest,allure
 from py.xml import html
 
-sys.path.append("F:\\automation")
+# sys.path.append("F:\\automation")
 sys.path.append('../../')
 
 def pytest_configure(config):
-    # 添加接口地址与项目名称
-    config._metadata["项目名称"] = "微站H5接口测试"
-    config._metadata['接口地址'] = 'https://sit-api.ixuenong.com/api/v3_3_1/wechat'
-    # 删除Java_Home
-    config._metadata.pop("JAVA_HOME")
+    """操作Environment"""
+    config._metadata["项目名称"] = "微站H5接口测试"  # 添加接口项目名称
+    config._metadata['接口地址'] = 'https://sit-api.ixuenong.com/api/v3_3_1/wechat' # 添加接口地址
+    config._metadata.pop("JAVA_HOME") # 删除Java_Home
+    config._metadata.pop("Packages")  # 删除Packages
+    config._metadata.pop("Platform")  # 删除Platform
+    config._metadata.pop("Plugins")  # 删除Plugins
+    config._metadata.pop("Python")  # 删除Python
 
 @pytest.mark.optionalhook
 def pytest_html_results_summary(prefix):
+    """添加测试数据人员信息"""
     prefix.extend([html.p("所属部门: 产研中心")])
     prefix.extend([html.p("测试人员: 张三")])
+
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_header(cells):
+    cells.insert(2, html.th('Description'))
+    cells.insert(3, html.th('Time', class_='sortable time', col='time'))
+    cells.pop()
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_row(report, cells):
+    cells.insert(2, html.td(report.description))
+    cells.insert(3, html.td(datetime.utcnow(), class_='col-time'))
+    cells.pop()
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    report.description = str(item.function.__doc__)
+    report.nodeid = report.nodeid.encode("utf-8").decode("unicode_escape")   #设置编码显示中文
 
--- testcase/test_teach_class.py
+++ testcase/test_teach_class.py
@@ -18,14 +18,14 @@ class Test_Courses(object):
         pass
 
     def test_get_teach_class_01(self):
-        """获取教学课接口"""
+        """获取教学课接口1"""
         data = dateChangeDict(filename, "teach_class", 1)  ##从excel表格里读取接口的数据
         data[5]['token'] = self.token_value[0]
         res = apiRequests(data[3], data[4], data[5], self.head)  ##发送接口请求
         print(res.json())
         assert res.json().get('msg') == data[6]
     def test_get_teach_class_02(self):
-        """获取教学课接口"""
+        """获取教学课接口2"""
         data = dateChangeDict(filename, "teach_class", 2)  ##从excel表格里读取接口的数据
         data[5]['token'] = self.token_value[0]
         res = apiRequests(data[3], data[4], data[5], self.head)  ##发送接口请求
@@ -33,7 +33,7 @@ class Test_Courses(object):
         assert res.json().get('msg') == data[6]
 
     def test_get_teach_class_03(self):
-        """获取教学课接口"""
+        """获取教学课接口3"""
         data = dateChangeDict(filename, "teach_class", 3)  ##从excel表格里读取接口的数据
         data[5]['token'] = self.token_value[0]
         res = apiRequests(data[3], data[4], data[5], self.head)  ##发送接口请求
@@ -41,7 +41,7 @@ class Test_Courses(object):
         assert res.json().get('msg') == data[6]
 
     def test_get_teach_class_04(self):
-        """获取教学课接口"""
+        """获取教学课接口4"""
         data = dateChangeDict(filename, "teach_class", 4)  ##从excel表格里读取接口的数据
         data[5]['token'] = self.token_value[0]
         res = apiRequests(data[3], data[4], data[5], self.head)  ##发送接口请求
@@ -49,7 +49,7 @@ class Test_Courses(object):
         assert res.json().get('msg') == data[6]
 
     def test_get_teach_class_05(self):
-        """获取教学课接口"""
+        """获取教学课接口5"""
         data = dateChangeDict(filename, "teach_class", 5)  ##从excel表格里读取接口的数据
         data[5]['token'] = self.token_value[0]
         res = apiRequests(data[3], data[4], data[5], self.head)  ##发送接口请求
@@ -57,7 +57,7 @@ class Test_Courses(object):
         assert res.json().get('msg') == data[6]
 
     def test_get_teach_class_06(self):
-        """获取教学课接口"""
+        """获取教学课接口6"""
         data = dateChangeDict(filename, "teach_class", 6)  ##从excel表格里读取接口的数据
         data[5]['token'] = self.token_value[0]
         res = apiRequests(data[3], data[4], data[5], self.head)  ##发送接口请求
